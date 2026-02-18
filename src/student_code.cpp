f43d80d6e81cecb8d38b19d6df138637fa24d0c4
zfiaz867 <zfiaz867@gmail.com>
2026-02-18 14:51:19 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -21,7 +21,7 @@ char getCharValue() {
     return var_char;
 }
 
-int convertFloatToInt(float value) {
+/*int convertFloatToInt(float value) {
     // convert value to int.
     return value;
 }
@@ -31,8 +31,8 @@ Convert Fahrenheit to Celsius and store your answer in a variable celsius.
 
 Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
-*/
+*
 float fahrenheitToCelsius(float fahrenheit) {
     //return celsius;
 
-}
+}*/
